4e5280d6334f2eb90d44cae97c789709f6e51cd2
unknown <trwg1@STMF096K32.Global.Tronox.Com>
2017-07-11 14:56:03 -0400

    qwe

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, ButtonType } from 'office-ui-fabric-react';
+import { Button, ButtonType, Label, DetailsList, PrimaryButton } from 'office-ui-fabric-react';
 import { Header } from './header';
 import { HeroList, HeroListItem } from './hero-list';
 import * as pnp from 'sp-pnp-js';
@@ -15,45 +15,18 @@ export interface AppState {
     loading: boolean;
     error: string;
     signedIn: boolean;
+    body: string;
+    from: string;
+    attachments: Array<any>
 }
 
 export class App extends React.Component<AppProps, AppState> {
     debugger;
+    private serviceRequest: any;
     private authCtx: adal.AuthenticationContext;
     constructor(props, context) {
         super(props, context);
         debugger;
-        this.state = {
-            listItems: [],
-            signedIn: false,
-            error: '',
-            loading: false,
-        };
-        var item = Office.context.mailbox.item;
-        var body = item.body;
-        // body.getAsync(
-        //     "text",
-        //     { asyncContext: "This is passed to the callback" },
-        //     function callback(result) {
-        //         debugger;
-        //     });
-        let bodytext:string="OKJH";
-   body.getAsync(
-            "text",
-            { asyncContext: "This is passed to the callback" },
-            (result)=> {
-                debugger;
-                bodytext=result;
-            }
-            
-            );
-
-
-        var attachments = item.attachments;
-        var firstAttachment = attachments[0];
-
-        var from = item.from;
-        var sender = from.emailAddress;
         const config: IAdalConfig = adalConfig;
         this.authCtx = new adal(config);
         window['Logging'] = {
@@ -66,8 +39,28 @@ export class App extends React.Component<AppProps, AppState> {
         if (!this.authCtx.isCallback(window.location.hash)) {
             this.authCtx.login();
         } else {
-            console.log('user is ' + this.authCtx.getCachedUser().userName);
             this.authCtx.handleWindowCallback();
+            console.log('user is ' + this.authCtx.getCachedUser().userName);
+            debugger;
+            this.state = {
+                listItems: [],
+                signedIn: false,
+                error: '',
+                loading: false,
+                body: '',
+                attachments: Office.context.mailbox.item.attachments,
+                from: Office.context.mailbox.item.from.emailAddress,
+            };
+            Office.context.mailbox.item.body.getAsync(
+                "html",
+                { asyncContext: "This is passed to the callback" },
+                (result) => {
+                    debugger;
+                    this.setState({ body: result.value });
+                }
+
+            );
+
             this.authCtx.acquireToken('f8f8d2ad-7c9d-4aac-80eb-3f00a263c879', (error, token) => {
                 if (error) {
                     console.log(error);
@@ -91,14 +84,97 @@ export class App extends React.Component<AppProps, AppState> {
             });
         }
     }
+    private renderBody() {
+        return { __html: this.state.body };
+    }
+    public editDocument(item: any): void {
+        debugger;
+        this.authCtx.acquireToken('f8f8d2ad-7c9d-4aac-80eb-3f00a263c879', (error, token) => {
+            debugger;
+            if (error) {
+                console.log(error);
+                return;
+            }
 
+            let myHeaders = new Headers();
+            debugger;
+            myHeaders.append('Authorization', 'Bearer ' + token);
+            let settings = {
+                method: 'GET',
+                headers: myHeaders,
+                mode: 'cors' as RequestMode,
+                cache: 'no-cache' as RequestCache
+            };
+            var url = Office.context.mailbox.restUrl + '/v2.0/me/messages';
+            fetch(url, settings).then((response) => {
+                debugger;
+            }).catch((err) => {
+                debugger;
+            })
+
+        });
+
+    }
+    private getAttachment(asyncResult) {
+        if (asyncResult.status === "succeeded") {
+            let myHeaders = new Headers();
+            debugger;
+            myHeaders.append('Authorization', 'Bearer ' + asyncResult.value);
+            let settings = {
+                method: 'GET',
+                headers: myHeaders,
+                mode: 'cors' as RequestMode,
+                cache: 'no-cache' as RequestCache
+            };
+            var url = Office.context.mailbox.restUrl + '/v2.0/me/MailboxSettings/AutomaticRepliesSetting';
+            fetch(url, settings).then((response) => {
+                debugger;
+            }).catch((err) => {
+                debugger;
+            })
+
+
+            //   makeServiceRequest();
+
+
+        }
+    }
     render() {
         debugger;
         if (this.authCtx.loginInProgress()) {
             return <div>logging in , please wait.</div>
         }
         return (
-            <div>logged in as {this.authCtx.getCachedUser().userName}</div>
+            <div>
+                <Label>Login info</Label>
+                <div>logged in as {this.authCtx.getCachedUser().userName}</div>
+                <Label>Message Text</Label>
+                <div dangerouslySetInnerHTML={this.renderBody()} />
+                <Label>Message From</Label>
+                <div>{this.state.from}</div>
+                <Label>Attachments</Label>
+                <DetailsList items={this.state.attachments}
+                    columns={[
+                        {
+                            key: "Edit", name: "", fieldName: "Title", minWidth: 20,
+                            onRender: (item) => <div>
+                                <i onClick={(e) => { this.editDocument(item); }}
+                                    className="ms-Icon ms-Icon--Edit" aria-hidden="true"></i>
+                            </div>
+                        },
+                        { key: "name", name: "name", fieldName: "name", minWidth: 20, maxWidth: 70 },
+                        { key: "size", name: "size", fieldName: "size", minWidth: 20, maxWidth: 70 },
+                        { key: "isInline", name: "isInline", fieldName: "isInline", minWidth: 20, maxWidth: 70 },
+                        { key: "attachmentType", name: "attachmentType", fieldName: "attachmentType", minWidth: 20, maxWidth: 70 },
+                        { key: "id", name: "id", fieldName: "id", minWidth: 20, maxWidth: 70 },
+
+                    ]}
+                />
+                <PrimaryButton href="#" icon="ms-Icon--Save">
+                    <i className="ms-Icon ms-Icon--Save" aria-hidden="true"></i>
+                    Save
+        </PrimaryButton>
+            </div>
         );
     };
 
